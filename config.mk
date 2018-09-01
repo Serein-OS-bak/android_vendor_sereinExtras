1fa8dab5c8698fed5a0e2fd0f5948963fc367351
Harsh Shandilya <harsh@prjkt.io>
2018-09-02 02:15:39 +0530

    pixelstyle: Default to 1080p bootanimation
    Stop making this an error for fuck's sake
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- config.mk
+++ config.mk
@@ -19,13 +19,14 @@ PRODUCT_PACKAGES += \
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/sereinextras/media/bootanimation_720.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/sereinextras/media/bootanimation_1080.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/sereinextras/media/bootanimation_1440.zip:system/media/bootanimation.zip
 else
-    $(error "sereinExtras: TARGET_BOOT_ANIMATION_RES is undefined")
+     $(warning "sereinExtras: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+     PRODUCT_COPY_FILES += vendor/sereinextras/media/bootanimation_1080.zip:system/media/bootanimation.zip
 endif
 
 # Fonts
