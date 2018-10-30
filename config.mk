vansh123456/android_vendor_revengeui

Add more bootanimations 1080: From walleye factory image 1440: From taimen factory image 720: From https://androidfilehost.com/?fid=745849072291684773

config.mk
@@ -24,8 +24,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.wallpapers_loc_request_suw=true
 
 # Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/pixelstyle/media/bootanimation.zip:system/media/bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
+else
+    $(error "PixelStyle: TARGET_BOOT_ANIMATION_RES is undefined")
+endif
 
 # Fonts
 PRODUCT_COPY_FILES += \

Serein-OS-bak/android_vendor_sereinExtras

config.mk
@@ -17,6 +17,17 @@ PRODUCT_PACKAGES += \
     WallpaperPickerGooglePrebuilt \
     NexusLauncherPrebuilt
 
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
+else
+    $(error "sereinExtras: TARGET_BOOT_ANIMATION_RES is undefined")
+endif
+
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/sereinextras/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
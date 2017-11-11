bc5902904079bf655671bb51f7a2503b958a11f1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-11 12:56:37 -0200

    Add more bootanimations 1080: From walleye factory image 1440: From taimen factory image 720: From https://androidfilehost.com/?fid=745849072291684773

--- config.mk
+++ config.mk
@@ -17,6 +17,17 @@ PRODUCT_PACKAGES += \
     WallpaperPickerGooglePrebuilt \
     NexusLauncherPrebuilt
 
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
+else
+    $(error "sereinExtras: TARGET_BOOT_ANIMATION_RES is undefined")
+endif
+
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/sereinextras/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
